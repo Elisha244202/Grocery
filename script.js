378d2ebfcab6d7aa76ba5ab50cc1d9830651135a
Elisha244202 <160017098+Elisha244202@users.noreply.github.com>
2024-02-19 18:47:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ menu.onclick = () => {
   console.log(document.querySelector(".menu"));
   document.querySelector(".header .menu").classList.toggle("active");
 };
+
+const search = document.querySelector(".fa-solid.fa-search");
+
+search.onclick = () => {
+  document.querySelector(".header .search").classList.toggle("active");
+  document.querySelector(".header .menu").classList.remove("active");
+};
